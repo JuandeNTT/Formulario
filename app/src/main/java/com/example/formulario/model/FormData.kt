ca7c33ff6211339b6c0061bcbbd4ea7dd1c9a3c9
juandentt <panchesmacias04@gmail.com>
2026-04-27 09:29:28 +0200

    Project creation

---
+++ app/src/main/java/com/example/formulario/model/FormData.kt
@@ -0,0 +1,9 @@
+package com.example.formulario.model
+
+data class FormData(
+    val title: String = "",
+    val description: String = "",
+    val category: String = "",
+    val priority: Int = 1,
+    val email: String = ""
+)
